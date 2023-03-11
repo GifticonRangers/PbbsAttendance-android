fdecac241f90df36da10c4e2563e24e4e1bfff5d
Lee YongIn <67788699+LeeYongIn0517@users.noreply.github.com>
2023-03-11 13:27:49 +0900

    FEAT: Modify tabLayout binding
    - viewPager2와 tablayoutItem 바인딩 오류 해결

--- presentation/src/main/java/com/example/pbbsattendance/view/StudentViewPagerFragment.kt
+++ presentation/src/main/java/com/example/pbbsattendance/view/StudentViewPagerFragment.kt
@@ -8,6 +8,9 @@ import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.example.pbbsattendance.R
+import com.example.pbbsattendance.adapter.ATTENDANCE_CHECK_PAGE_INDEX
+import com.example.pbbsattendance.adapter.ATTENDANCE_HISTORY_PAGE_INDEX
+import com.example.pbbsattendance.adapter.ATTENDANCE_STATUS_PAGE_INDEX
 import com.example.pbbsattendance.adapter.StudentViewPagerAdapter
 import com.example.pbbsattendance.databinding.FragmentStudentViewPagerBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -24,10 +27,24 @@ class StudentViewPagerFragment : Fragment() {
     ): View? {
         _binding = FragmentStudentViewPagerBinding.inflate(inflater, container, false)
         val viewPager = binding.viewpagerStudent
+        val tabLayout = binding.tabLayout
 
         viewPager.adapter = StudentViewPagerAdapter(this)
 
+        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+            tab.text = getTabTitle(position)
+        }.attach()
+
         return binding.root
+    }
+
+    private fun getTabTitle(position: Int): String?{
+        return when(position) {
+            ATTENDANCE_CHECK_PAGE_INDEX -> "출결 관리"
+            ATTENDANCE_STATUS_PAGE_INDEX -> "출결 현황"
+            ATTENDANCE_HISTORY_PAGE_INDEX -> "출결 기록"
+            else -> null
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
