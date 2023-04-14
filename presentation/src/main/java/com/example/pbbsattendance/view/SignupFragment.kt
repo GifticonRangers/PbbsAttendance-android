0e8322548f4ba3d7376a0a8f215ff6998d857f28
이용인 <yongin0517@gmail.com>
2023-04-15 01:25:59 +0900

    FEAT: Modify Signup feature
    회원가입 기능 완료

--- presentation/src/main/java/com/example/pbbsattendance/view/SignupFragment.kt
+++ presentation/src/main/java/com/example/pbbsattendance/view/SignupFragment.kt
@@ -9,12 +9,11 @@ import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.example.domain.model.dto.UserDto
 import com.example.pbbsattendance.R
 import com.example.pbbsattendance.databinding.FragmentSignupBinding
 import com.example.pbbsattendance.viewmodel.SignupViewModel
@@ -26,7 +25,7 @@ class SignupFragment:Fragment() {
     lateinit var  navController: NavController
     private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
-    private val viewModel by viewModels<SignupViewModel>()
+    private val sinupViewModel by viewModels<SignupViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,13 +38,26 @@ class SignupFragment:Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = viewModel
+        binding.viewModel = sinupViewModel
         navController = this@SignupFragment.findNavController()
 
 
         binding.apply {
             buttonSignup.setOnClickListener {
-                view.findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
+                val genderType = checkGenderType()
+                val userType = checkUserType()
+                val dto = UserDto(
+                    id = null,
+                    idUser = inputSignupId.text.toString(),
+                    pwUser = inputPwConfirm.text.toString(),
+                    name = inputName.text.toString(),
+                    phone = inputPhoneNumber.text.toString(),
+                    email= inputEmail.text.toString(),
+                    department = inputDepartment.text.toString(),
+                    type = genderType,
+                    gender = userType
+                )
+                viewModel?.signup(dto)
             }
             buttonCheckId.setOnClickListener {
                 val result = inputSignupId.text.toString()
@@ -57,16 +69,35 @@ class SignupFragment:Fragment() {
                     false -> checkIdError.visibility = View.VISIBLE
                 }
             })
+            viewModel?.signupResult?.observe(viewLifecycleOwner, Observer {
+                view.findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
+            })
             inputSignupId.doOnTextChanged { text, start, before, count ->
                 checkIdSuccess.visibility = View.INVISIBLE
                 checkIdError.visibility = View.INVISIBLE
             }
-            
+
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun checkGenderType():Int{
+        var result = 0
+        if (binding.buttonFemale.isChecked) result = 1
+        if (binding.buttonMale.isChecked) result = 0
+
+        return result
+    }
+
+    fun checkUserType():Int{
+        var result = 0
+        if (binding.buttonProfessor.isChecked) result = 1
+        if (binding.buttonStudent.isChecked) result = 2
+
+        return result
     }
 }
--- presentation/src/main/java/com/example/pbbsattendance/viewmodel/SignupViewModel.kt
+++ presentation/src/main/java/com/example/pbbsattendance/viewmodel/SignupViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.model.dto.UserDto
 import com.example.data.repository.LoginRepositoryImpl
+import com.example.domain.model.UserModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,14 +16,23 @@ import javax.inject.Inject
 class SignupViewModel @Inject constructor(private val repository:LoginRepositoryImpl):ViewModel(){
     private var _checkIdResult = MutableLiveData<Boolean>()
     val checkIdResult:LiveData<Boolean> get() = _checkIdResult
+    private var _signupResult = MutableLiveData<UserModel>()
+    val signupResult:LiveData<UserModel> get() = _signupResult
+
     fun checkId(id:String){
-        Log.i("id", id)
         viewModelScope.launch {
             val result = repository.checkId(id)
             when (result){
                 true -> _checkIdResult.value = true
                 false -> _checkIdResult.value = false
             }
+        }
+    }
+
+    fun signup(dto: UserDto){
+        viewModelScope.launch {
+            val result = repository.signup(dto)
+            _signupResult.value = result
         }
     }
 }
