0313de7c7454a0f100a47a894817d57bec7ca09a
이용인 <yongin0517@gmail.com>
2023-04-21 01:05:11 +0900

    FEAT: Create navigation professor screen and student screen

--- presentation/src/main/java/com/example/pbbsattendance/view/HomeFragment.kt
+++ presentation/src/main/java/com/example/pbbsattendance/view/HomeFragment.kt
@@ -6,23 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import com.example.data.repository.SubjectRepositoryImpl
+import com.example.domain.model.UserModel
 import com.example.domain.model.dto.IdDto
-import com.example.domain.usecases.GetUserUseCase
+import com.example.domain.model.type.TypeUser
 import com.example.pbbsattendance.R
 import com.example.pbbsattendance.databinding.FragmentHomeBinding
 import com.example.pbbsattendance.util.mapPepTalk
 import com.example.pbbsattendance.util.mapUserType
 import com.example.pbbsattendance.viewmodel.HomeViewModel
-import com.example.pbbsattendance.viewmodel.LoginViewModel
+import com.github.tlaabs.timetableview.Schedule
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.ArrayList
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -31,6 +32,7 @@ class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private val homeViewModel by viewModels<HomeViewModel>()
+    lateinit var userData: UserModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -54,27 +56,23 @@ class HomeFragment : Fragment() {
                 view.findNavController().navigate(R.id.action_homeFragment_to_lectureAddFragment)
             }
 
-            icEditSchedule.setOnClickListener {
-                //view.findNavController().navigate(R.id.action_homeFragment_to_studentViewPagerFragment)
-                view.findNavController().navigate(R.id.action_homeFragment_to_professorViewPagerFragment)
-            }
-            viewModel?.user?.observe(viewLifecycleOwner, Observer {
-                viewModel?.showScheduleSubjects(IdDto(it.id!!))
-                userName.text = it.nameUser
-                userType.text = mapUserType(it.typeUser?.koName)
-                peptalk.text = mapPepTalk(it.typeUser?.koName)
-            })
-            viewModel?.scheduleSubjectsResult?.observe(viewLifecycleOwner, Observer {
-                Log.i("scheduleSubjectsResult","timetable create")
-                it.forEach {
-                    Log.i("Result.classTitle","${it.classTitle}")
-                    Log.i("Result.day","${it.day}")
-                    Log.i("Result.startTime","${it.startTime}")
-                    Log.i("Result.endTime","${it.endTime}")
-                    Log.i("Result.professorName","${it.professorName}")
-                    Log.i("Result.classPlace","${it.classPlace}")
+            timetable.setOnStickerSelectEventListener{ i: Int, schedules: ArrayList<Schedule> ->
+                when(userData.typeUser){
+                    TypeUser.PROFESSOR -> view.findNavController().navigate(R.id.action_homeFragment_to_professorViewPagerFragment)
+                    TypeUser.STUDENT -> view.findNavController().navigate(R.id.action_homeFragment_to_studentViewPagerFragment)
+                    else -> null
                 }
 
+            }
+
+            viewModel?.user?.observe(viewLifecycleOwner, Observer {
+                viewModel?.showScheduleSubjects(IdDto(it.id!!))
+                userData = it
+                userName.text = userData.nameUser
+                userType.text = mapUserType(userData.typeUser?.koName)
+                peptalk.text = mapPepTalk(userData.typeUser?.koName)
+            })
+            viewModel?.scheduleSubjectsResult?.observe(viewLifecycleOwner, Observer {
                 timetable.add(it)
             })
         }
