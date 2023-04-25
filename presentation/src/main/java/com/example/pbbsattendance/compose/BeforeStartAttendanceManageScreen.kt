f01b93342d01e817cacbdc2dc5f435e81fa1dffe
이용인 <yongin0517@gmail.com>
2023-04-25 19:04:34 +0900

    FEAT: Modified BeforeStartAttendanceManageScreen UI structure

--- presentation/src/main/java/com/example/pbbsattendance/compose/BeforeStartAttendanceManageScreen.kt
+++ presentation/src/main/java/com/example/pbbsattendance/compose/BeforeStartAttendanceManageScreen.kt
@@ -3,97 +3,99 @@ package com.example.pbbsattendance.compose
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.domain.model.dto.IdDto
+import com.example.domain.model.dto.UserSubjectDto
 import com.example.pbbsattendance.compose.component.LectureTitle
+import com.example.pbbsattendance.model.LectureTimeItemModel
 import com.example.pbbsattendance.ui.theme.*
+import com.example.pbbsattendance.viewmodel.AfterStartAttendanceManageViewModel
 import com.example.pbbsattendance.viewmodel.MainViewModel
+import com.islandparadise14.mintable.ScheduleEntity
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun BeforeStartAttendanceManageScreen(
     navController: NavController,
-    viewModel: MainViewModel = hiltViewModel()
+    mainViewModel: MainViewModel = hiltViewModel(),
+    afterStartAttendanceManageViewModel: AfterStartAttendanceManageViewModel = hiltViewModel(),
 ) {
-    val scheduleSubject = viewModel.getScheduleSubject()
+    val scheduleSubject = mainViewModel.getScheduleSubject()
+    val user = mainViewModel.getUser()
+    afterStartAttendanceManageViewModel.getAttendanceDateList(UserSubjectDto(user.id,scheduleSubject.originId))
+    afterStartAttendanceManageViewModel.getStudentList(IdDto(scheduleSubject.originId))
 
-    Column(
-        Modifier
-            .background(color = Color.White)
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    val dateList by afterStartAttendanceManageViewModel.dateList.observeAsState(initial = emptyList())
+    BeforeStartAttendanceManageScreen(dateList = dateList, scheduleSubject = scheduleSubject, onStartAttendance = {navController.navigate(route = Screen.AfterStartAttendanceManage.route)})
+
+
+}
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+fun BeforeStartAttendanceManageScreen(dateList:List<LectureTimeItemModel>, scheduleSubject: ScheduleEntity, onStartAttendance:()->Unit = {}){
+    val state = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+    val scope = rememberCoroutineScope()
+
+    ModalBottomSheetLayout(
+        sheetState = state,
+        sheetContent = {
+            LectureTimeModalContent(state,dateList)
+        },
     ) {
-        LectureTitle(title = scheduleSubject.scheduleName)
-        Button(
-            onClick = { navController.navigate(route = Screen.AfterStartAttendanceManage.route) },
-            colors = ButtonDefaults.buttonColors(backgroundColor = Blue4),
-            border = BorderStroke(1.dp, Blue3),
-            shape = RoundedCornerShape(12.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 68.dp, vertical = 36.dp)
-                .height(55.dp)
-
+        Column(
+            Modifier
+                .background(color = Color.White)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "출석 시작",
-                style = TextStyle(
-                    fontFamily = suit_regular,
-                    fontWeight = FontWeight.W400,
-                    fontSize = 16.sp
-                ),
-                color = Grey
-            )
-        }
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .drawBehind {
-                    drawLine(
-                        color = Grey4,
-                        start = Offset(0f, size.height),
-                        end = Offset(size.width, size.height),
-                        strokeWidth = 10f
-                    )
-                }
-        ){}
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(top = 12.dp, bottom = 12.dp, start = 10.dp)
-        ){
-            Text(text = "학생", style = TextStyle(fontFamily = suit_regular, fontWeight = FontWeight.W500, fontSize = 12.sp), color = Grey)
-            Text(text = "24", style = TextStyle(fontFamily = suit_regular, fontWeight = FontWeight.W500, fontSize = 12.sp), color = Blue3, modifier = Modifier.padding(start = 5.dp))
-        }
-        LazyColumn {
-//            studentListStateContent?.let {
-//                itemsIndexed(
-//                    it.content
-//                ) { index, item ->
-//                    StudentCard(data = item)
-//                    Log.i("Attendance", item.name)
-//                }
-//            }
+            LectureTitle(title = scheduleSubject.scheduleName)
+            Button(
+                onClick = { scope.launch { state.show() } },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Blue4),
+                border = BorderStroke(1.dp, Blue3),
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 68.dp, vertical = 36.dp)
+                    .height(55.dp)
+
+            ) {
+                Text(
+                    text = "출석 시작",
+                    style = TextStyle(
+                        fontFamily = suit_regular,
+                        fontWeight = FontWeight.W400,
+                        fontSize = 16.sp
+                    ),
+                    color = Grey
+                )
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp, bottom = 12.dp, start = 10.dp)
+            ){
+
+            }
         }
     }
 }
-
 //@Preview
 //@Composable
 //private fun BeforeStartAttendanceManagePreview(){
