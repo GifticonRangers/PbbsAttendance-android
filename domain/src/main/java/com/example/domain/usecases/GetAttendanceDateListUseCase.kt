e8339e99d35893773a8a0fb31ac22b3fa6015f5b
이용인 <yongin0517@gmail.com>
2023-04-25 18:21:26 +0900

    RENAME

--- domain/src/main/java/com/example/domain/usecases/GetAttendanceDateListUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/GetAttendanceDateListUseCase.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class GetAttendanceDateList @Inject constructor(private val attendanceRepository: AttendanceRepository) {
+class GetAttendanceDateListUseCase @Inject constructor(private val attendanceRepository: AttendanceRepository) {
     suspend fun invoke(dto:UserSubjectDto):ArrayList<LectureDateModel>{
         val result = attendanceRepository.showAttendanceTimeList(dto)
         return result
