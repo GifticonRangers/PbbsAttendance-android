d1659c72997acc70034e2076bceb38e6c34d2d18
이용인 <yongin0517@gmail.com>
2023-04-25 18:27:50 +0900

    FEAT: Modify StudentWithSwitchCard component
    -switch를 이미지가 아닌 android api로 변경함.
    -추후 디자인 및 매퍼함수 적용예정

--- presentation/src/main/java/com/example/pbbsattendance/compose/component/StudentWithSwitchCard.kt
+++ presentation/src/main/java/com/example/pbbsattendance/compose/component/StudentWithSwitchCard.kt
@@ -1,50 +1,69 @@
 package com.example.pbbsattendance.compose.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.domain.model.UserModel
+import com.example.domain.model.type.GenderUser
+import com.example.domain.model.type.TypeUser
 import com.example.pbbsattendance.R
-import com.example.pbbsattendance.ui.theme.Grey
-import com.example.pbbsattendance.ui.theme.Grey2
-import com.example.pbbsattendance.ui.theme.suit_regular
+import com.example.pbbsattendance.ui.theme.*
 
-//@Composable
-//fun StudentWithSwitchCard(data: Student) {
-//    Row(
-//        Modifier
-//            .padding(bottom = 5.dp)
-//            .fillMaxWidth(),
-//        horizontalArrangement = Arrangement.SpaceBetween,
-//        verticalAlignment = Alignment.CenterVertically
-//    ) {
-//        Row(
-//            verticalAlignment = Alignment.CenterVertically
-//        ) {
-//            Image(painter = painterResource(id = R.drawable.ic_student), contentDescription = "")
-//            Text(text = data.name, style = TextStyle(fontFamily = suit_regular, fontWeight = FontWeight.W500, fontSize = 12.sp), color = Grey, modifier = Modifier.padding(start=5.dp)  )
-//            Text(text = data.studentId, style = TextStyle(fontFamily = suit_regular, fontWeight = FontWeight.W500, fontSize = 10.sp), color = Grey2, modifier = Modifier.padding(start=5.dp) )
-//        }
-//        Row(
-//        ) {
-//            //Image(painter = painterResource(id = attendanceImageMaaper(data.isAttendance)), contentDescription = "")
-//        }
-//    }
-//}
-//
-//@Preview
-//@Composable
-//fun StudentWithSwitchCardPreview() {
-//    StudentWithSwitchCard(data = Student(name = "김문기", studentId = "20202001762", attendanceState = "출석완료"))
-//}
+@Composable
+fun StudentWithSwitchCard(data: UserModel) {
+    val checkedSwitch = remember{mutableStateOf(false)}
+    Row(
+        Modifier
+            .padding(bottom = 5.dp)
+            .background(color = Color.White)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(painter = painterResource(id = R.drawable.ic_student), contentDescription = "")
+            Text(text = data.nameUser, style = TextStyle(fontFamily = suit_regular, fontWeight = FontWeight.W500, fontSize = 12.sp), color = Grey, modifier = Modifier.padding(start=5.dp)  )
+            Text(text = data.idUser, style = TextStyle(fontFamily = suit_regular, fontWeight = FontWeight.W500, fontSize = 10.sp), color = Grey2, modifier = Modifier.padding(start=5.dp) )
+        }
+        Row(
+        ) {
+            Switch(
+                checked = checkedSwitch.value,
+                onCheckedChange = {checkedSwitch.value = it},
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = Blue3,
+                    checkedTrackColor = Blue4,
+                    uncheckedThumbColor = Grey2,
+                    uncheckedTrackColor = Grey
+                )
+            )
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun StudentWithSwitchCardPreview() {
+    StudentWithSwitchCard(data = UserModel(0,"202001488",TypeUser.STUDENT,"김문기,","01076867103","000lyi@inu.ac.kr","컴퓨터공학부", GenderUser.MALE))
+}
