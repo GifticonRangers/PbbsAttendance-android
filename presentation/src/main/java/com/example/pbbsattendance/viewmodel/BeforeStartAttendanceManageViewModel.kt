288e817050dbdf7720b9f0e4bee863837233811c
이용인 <yongin0517@gmail.com>
2023-04-25 19:51:40 +0900

    RENAME

--- presentation/src/main/java/com/example/pbbsattendance/viewmodel/BeforeStartAttendanceManageViewModel.kt
+++ presentation/src/main/java/com/example/pbbsattendance/viewmodel/BeforeStartAttendanceManageViewModel.kt
@@ -17,7 +17,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AfterStartAttendanceManageViewModel @Inject constructor(
+class BeforeStartAttendanceManageViewModel @Inject constructor(
     private val getAttendanceDateListUseCase: GetAttendanceDateListUseCase,
     private val getStudentListUseCase: GetStudentListUseCase
 ): ViewModel(){
