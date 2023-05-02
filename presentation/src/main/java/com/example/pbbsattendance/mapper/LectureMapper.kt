13bbe598c665ff850fd0a15b732b3b28ac23c534
이용인 <yongin0517@gmail.com>
2023-05-03 01:29:55 +0900

    FEAT: Modified LectureMapper
    스트링 값 추가(추가 안하면 500에러남)

--- presentation/src/main/java/com/example/pbbsattendance/mapper/LectureMapper.kt
+++ presentation/src/main/java/com/example/pbbsattendance/mapper/LectureMapper.kt
@@ -15,8 +15,8 @@ object LectureMapper {
 
     fun mapToLectureInfoDto(lectureTimeItemModel: LectureTimeItemModel, idSubject:Int):LectureInfoDto{
         return LectureInfoDto(
-            timeAttendance = lectureTimeItemModel.time,
-            weekAttendance = lectureTimeItemModel.date,
+            timeAttendance = lectureTimeItemModel.time + "차시",
+            weekAttendance = lectureTimeItemModel.week + "주차",
             idSubject = idSubject.toString()
         )
     }
